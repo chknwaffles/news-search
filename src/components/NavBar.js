2147288401402e7f24e1cf0473195a56f13d0cb5
chknwaffles <wonsae.kim@gmail.com>
2019-07-05 03:52:23 -0400

    add material ui and navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export default function NavBar() {
+
+    return (
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <Button className='more-options'> <MoreVertIcon /> </Button>
+                </Toolbar> 
+            </AppBar>
+            
+        </div>
+    )
+}
