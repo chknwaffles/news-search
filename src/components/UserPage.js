2d9639f968b2e721404c3a2e0d459263d649846b
Won Kim <wonsae.kim@gmail.com>
2019-07-09 17:06:29 -0400

    auto-login and profile fix

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
-export default function UserPage() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function UserPage(props) {
+    const classes = useStyles();
+    const [checked, setChecked] = useState([]);
+  
+    const renderLikedArticles = () => {
+        return props.currentUser.articles.map((article, i) => {
+            return (
+                <ListItem key={i} role={undefined} dense button>
+                    <ListItemText id={article.id} primary={article.title} onClick={() => window.open(article.url, '_blank')} />
+                </ListItem>
+            )
+        })
+    }
+
     return (
-        <div>
-        User show page
-        </div>
-    )
+        <List className={classes.root}>
+            {renderLikedArticles()}
+        </List>
+    );
 }
